120cca790c2d4f40d5161abebc128f55f58eccd8
kylemede <kylemede@gmail.com>
2016-09-01 18:00:10 +0900

    modernized to Python 3.#

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
+from __future__ import absolute_import
 import warnings
 warnings.simplefilter("error")
 
-from kmLogger import getLogger
-from kmLogger import setUpLogger
+from .kmLogger import getLogger
+from .kmLogger import setUpLogger
