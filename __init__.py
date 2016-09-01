18cbc0a68d181fdd8762837f3251611298fbd5d9
kylemede <kylemede@gmail.com>
2016-09-01 17:31:37 +0900

    fixed extra imports in init

--- __init__.py
+++ __init__.py
@@ -3,7 +3,3 @@ warnings.simplefilter("error")
 
 from kmLogger import getLogger
 from kmLogger import setUpLogger
-from kmLogger import logSystemInfo
-from kmLogger import addFileHandler
-from kmLogger import addStreamHandler
-from kmLogger import logDict
